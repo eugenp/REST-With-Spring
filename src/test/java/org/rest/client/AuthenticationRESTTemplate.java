fa3cf3ae507e05f6124d439e7e75c8a3b161292d
eugenp <hanriseldon@gmail.com>
2012-02-15 18:02:01 +0200

    intermediary work on using the Spring RestTempalte

---
+++ src/test/java/org/rest/client/AuthenticationRESTTemplate.java
@@ -0,0 +1,40 @@
+package org.rest.client;
+
+import org.rest.sec.dto.User;
+import org.rest.testing.ExamplePaths;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AuthenticationRESTTemplate{
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private ExamplePaths paths;
+	
+	//
+	
+	public final ResponseEntity< User > authenticate(){
+		return restTemplate.exchange( paths.getAuthenticationUri(), HttpMethod.POST, new HttpEntity< String >( createHeaders() ), User.class );
+	}
+
+	// util
+	
+	final HttpHeaders createHeaders(){
+		final HttpHeaders headers = new HttpHeaders(){
+			{
+				set( com.google.common.net.HttpHeaders.ACCEPT, MediaType.APPLICATION_XML_VALUE );
+			}
+		};
+		return headers;
+	}
+	
+}
--- src/test/java/org/rest/security/ClientRESTIntegrationTest.java
+++ src/test/java/org/rest/security/ClientRESTIntegrationTest.java
@@ -5,43 +5,41 @@ import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.rest.client.AuthenticationRESTTemplate;
+import org.rest.client.UserRESTTemplate;
 import org.rest.sec.dto.User;
 import org.rest.spring.application.ApplicationTestConfig;
 import org.rest.spring.persistence.jpa.PersistenceJPAConfig;
 import org.rest.test.AbstractRESTIntegrationTest;
-import org.rest.testing.ExamplePaths;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.web.client.RestTemplate;
 
 @RunWith( SpringJUnit4ClassRunner.class )
 @ContextConfiguration( classes = { ApplicationTestConfig.class, PersistenceJPAConfig.class },loader = AnnotationConfigContextLoader.class )
+@SuppressWarnings( "rawtypes" )
+@Ignore( "TODO: temporarily disabled" )
 public class ClientRESTIntegrationTest extends AbstractRESTIntegrationTest{
 	
 	@Autowired
-	private ExamplePaths paths;
+	private AuthenticationRESTTemplate authenticationRestTemplate;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private UserRESTTemplate userRESTTemplate;
 	
 	// tests
 	
 	// GET
 	
-	@SuppressWarnings( "rawtypes" )
 	@Test
 	public final void whenGetUsersWithAuthentication_then200IsReceived(){
 		// When
-		final ResponseEntity< List > response = restTemplate.exchange( paths.getUserUri(), HttpMethod.GET, new HttpEntity< String >( createHeaders() ), List.class );
+		final ResponseEntity< List > response = userRESTTemplate.findAll();
 		
 		// Then
 		assertThat( response.getStatusCode().value(), is( 200 ) );
@@ -50,21 +48,10 @@ public class ClientRESTIntegrationTest extends AbstractRESTIntegrationTest{
 	@Test
 	public final void whenAuthenticating_then201IsReceived(){
 		// When
-		final ResponseEntity< User > response = restTemplate.exchange( paths.getAuthenticationUri(), HttpMethod.POST, new HttpEntity< String >( createHeaders() ), User.class );
+		final ResponseEntity< User > response = authenticationRestTemplate.authenticate();
 		
 		// Then
 		assertThat( response.getStatusCode().value(), is( 201 ) );
-	}
-	
-	//
-	
-	final HttpHeaders createHeaders(){
-		final HttpHeaders headers = new HttpHeaders(){
-			{
-				set( "Accept", MediaType.APPLICATION_XML_VALUE );
-			}
-		};
-		return headers;
 	}
 	
 }
