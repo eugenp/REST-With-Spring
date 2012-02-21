98bddc7eb3e5b26e9a76a87c1d7929f57ba9fc77
eugenp <hanriseldon@gmail.com>
2012-02-21 18:53:19 +0200

    authentication work

--- src/test/java/org/rest/sec/client/AuthenticationRESTTemplate.java
+++ src/test/java/org/rest/sec/client/AuthenticationRESTTemplate.java
@@ -1,49 +1,53 @@
 package org.rest.sec.client;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.commons.codec.binary.Base64;
 import org.rest.sec.dto.User;
 import org.rest.testing.ExamplePaths;
+import org.rest.testing.marshaller.IMarshaller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AuthenticationRESTTemplate{
 	
+	@Autowired @Qualifier( "xstreamMarshaller" ) IMarshaller marshaller;
 	@Autowired private RestTemplate restTemplate;
+
 	@Autowired private ExamplePaths paths;
 	
 	@Value( "${port}" ) private int port;
 	@Value( "${host}" ) private String host;
 	
+	
 	//
 	
-	public final ResponseEntity< User > authenticate( final String user, final String pass ){
-		final HttpComponentsClientHttpRequestFactory requestFactory = (HttpComponentsClientHttpRequestFactory) restTemplate.getRequestFactory();
-		final DefaultHttpClient httpClient = (DefaultHttpClient) requestFactory.getHttpClient();
-		httpClient.getCredentialsProvider().setCredentials( new AuthScope( host, port, AuthScope.ANY_REALM ), new UsernamePasswordCredentials( user, pass ) );
-		
-		return restTemplate.exchange( paths.getAuthenticationUri(), HttpMethod.POST, new HttpEntity< String >( createHeaders() ), User.class );
+	public final ResponseEntity< User > authenticate( final String username, final String password ){
+		return restTemplate.exchange( getURI(), HttpMethod.POST, new HttpEntity< User >( createHeaders( username, password ) ), User.class );
 	}
 	
 	// util
 	
-	final HttpHeaders createHeaders(){
-		final HttpHeaders headers = new HttpHeaders(){
+	final HttpHeaders createHeaders( final String username, final String password ){
+		return new HttpHeaders(){
 			{
-				set( com.google.common.net.HttpHeaders.ACCEPT, MediaType.APPLICATION_XML_VALUE );
+				set( com.google.common.net.HttpHeaders.ACCEPT, marshaller.getMime() );
+				
+				final String authorisation = username + ":" + password;
+				final byte[] encodedAuthorisation = Base64.encodeBase64( authorisation.getBytes() );
+				set( "Authorization", "Basic " + new String( encodedAuthorisation ) );
 			}
 		};
-		return headers;
+	}
+	
+	final String getURI(){
+		return paths.getAuthenticationUri();
 	}
 	
 }
