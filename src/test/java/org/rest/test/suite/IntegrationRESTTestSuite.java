7072156419e4dc2968a6b3cd5dc0ebaa85786368
eugen-elanross <eugen@elanross.com>
2012-03-11 13:07:09 +0200

    persistence work

--- src/test/java/org/rest/test/suite/IntegrationRESTTestSuite.java
+++ src/test/java/org/rest/test/suite/IntegrationRESTTestSuite.java
@@ -6,7 +6,15 @@ import org.junit.runners.Suite.SuiteClasses;
 import org.rest.security.SecurityRESTIntegrationTest;
 
 @RunWith( Suite.class )
-@SuiteClasses( { IntegrationClientRESTTestSuite.class, SecurityRESTIntegrationTest.class, IntegrationDiscoverabilityRESTTestSuite.class, IntegrationLogicRESTTestSuite.class, IntegrationPaginationRESTTestSuite.class, IntegrationSortRESTTestSuite.class, IntegrationMimeRESTTestSuite.class } )
+@SuiteClasses( {// @formatter:off
+	IntegrationClientRESTTestSuite.class,
+	IntegrationDiscoverabilityRESTTestSuite.class,
+	IntegrationLogicRESTTestSuite.class,
+	IntegrationPaginationRESTTestSuite.class,
+	IntegrationSortRESTTestSuite.class,
+	IntegrationMimeRESTTestSuite.class,
+	SecurityRESTIntegrationTest.class
+} ) // @formatter:on
 public final class IntegrationRESTTestSuite{
 	//
 }
