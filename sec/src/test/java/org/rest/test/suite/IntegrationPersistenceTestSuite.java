a6cd21fea735a128b29aa143e6bcdc2728cf12e7
eugenp <hanriseldon@gmail.com>
2012-08-06 17:36:30 +0300

    DAO tests removed as unnecessary

--- sec/src/test/java/org/rest/test/suite/IntegrationPersistenceTestSuite.java
+++ sec/src/test/java/org/rest/test/suite/IntegrationPersistenceTestSuite.java
@@ -3,9 +3,6 @@ package org.rest.test.suite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.rest.sec.persistence.dao.PrincipalDAOPersistenceIntegrationTest;
-import org.rest.sec.persistence.dao.PrivilegeDAOPersistenceIntegrationTest;
-import org.rest.sec.persistence.dao.RoleDAOPersistenceIntegrationTest;
 import org.rest.sec.persistence.service.PrincipalSearchPersistenceIntegrationTest;
 import org.rest.sec.persistence.service.PrincipalServicePersistenceIntegrationTest;
 import org.rest.sec.persistence.service.PrivilegeSearchPersistenceIntegrationTest;
@@ -15,11 +12,10 @@ import org.rest.sec.persistence.service.RoleServicePersistenceIntegrationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({// @formatter:off
-PrincipalSearchPersistenceIntegrationTest.class, PrincipalServicePersistenceIntegrationTest.class, PrincipalDAOPersistenceIntegrationTest.class,
-
-PrivilegeSearchPersistenceIntegrationTest.class, PrivilegeServicePersistenceIntegrationTest.class, PrivilegeDAOPersistenceIntegrationTest.class,
-
-RoleSearchPersistenceIntegrationTest.class, RoleServicePersistenceIntegrationTest.class, RoleDAOPersistenceIntegrationTest.class })
+    PrincipalSearchPersistenceIntegrationTest.class, PrincipalServicePersistenceIntegrationTest.class,
+    PrivilegeSearchPersistenceIntegrationTest.class, PrivilegeServicePersistenceIntegrationTest.class,
+    RoleSearchPersistenceIntegrationTest.class, RoleServicePersistenceIntegrationTest.class
+})
 // @formatter:on
 public final class IntegrationPersistenceTestSuite {
     //
