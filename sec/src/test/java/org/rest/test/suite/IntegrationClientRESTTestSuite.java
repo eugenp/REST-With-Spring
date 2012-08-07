2a58c2e4d9b243c9813eaad3cd21d0375baf0359
eugenp <hanriseldon@gmail.com>
2012-08-07 14:35:02 +0300

    adding tests to the manual suites

--- sec/src/test/java/org/rest/test/suite/IntegrationClientRESTTestSuite.java
+++ sec/src/test/java/org/rest/test/suite/IntegrationClientRESTTestSuite.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.rest.sec.client.template.test.AuthenticationClientRESTIntegrationTest;
+import org.rest.sec.client.template.test.PrivilegeClientRESTIntegrationTest;
+import org.rest.sec.client.template.test.RoleClientRESTIntegrationTest;
 import org.rest.sec.client.template.test.UserClientRESTIntegrationTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UserClientRESTIntegrationTest.class, AuthenticationClientRESTIntegrationTest.class })
+@SuiteClasses({ UserClientRESTIntegrationTest.class, RoleClientRESTIntegrationTest.class, PrivilegeClientRESTIntegrationTest.class, AuthenticationClientRESTIntegrationTest.class })
 public final class IntegrationClientRESTTestSuite {
     //
 }
