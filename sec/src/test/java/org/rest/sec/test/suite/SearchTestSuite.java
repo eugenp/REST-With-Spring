e502c7d5c178660efff751def41e53e4f774a60c
eugenp <hanriseldon@gmail.com>
2012-08-09 17:14:22 +0300

    integration testing fixes

--- sec/src/test/java/org/rest/sec/test/suite/SearchTestSuite.java
+++ sec/src/test/java/org/rest/sec/test/suite/SearchTestSuite.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.rest.common.search.ConstructQueryStringUnitTest;
-import org.rest.common.util.ParseQueryStringUnitTest;
 import org.rest.sec.persistence.service.PrincipalSearchPersistenceIntegrationTest;
 import org.rest.sec.persistence.service.PrivilegeSearchPersistenceIntegrationTest;
 import org.rest.sec.persistence.service.RoleSearchPersistenceIntegrationTest;
@@ -13,11 +12,13 @@ import org.rest.sec.web.role.RoleSearchRESTIntegrationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({// @formatter:off
-ParseQueryStringUnitTest.class, ConstructQueryStringUnitTest.class,
-
-PrincipalSearchPersistenceIntegrationTest.class, RoleSearchPersistenceIntegrationTest.class, PrivilegeSearchPersistenceIntegrationTest.class,
-
-RoleSearchRESTIntegrationTest.class, PrivilegeSearchRESTIntegrationTest.class })
+    ConstructQueryStringUnitTest.class,
+    PrincipalSearchPersistenceIntegrationTest.class,
+    RoleSearchPersistenceIntegrationTest.class,
+    PrivilegeSearchPersistenceIntegrationTest.class,
+    RoleSearchRESTIntegrationTest.class,
+    PrivilegeSearchRESTIntegrationTest.class
+})
 // @formatter:on
 public final class SearchTestSuite {
     //
--- sec/src/test/java/org/rest/sec/test/suite/UnitTestSuite.java
+++ sec/src/test/java/org/rest/sec/test/suite/UnitTestSuite.java
@@ -4,13 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.rest.common.search.ConstructQueryStringUnitTest;
-import org.rest.common.util.ParseQueryStringUnitTest;
 import org.rest.sec.persistence.service.impl.PrincipalServiceUnitTest;
 import org.rest.sec.persistence.service.impl.PrivilegeServiceUnitTest;
 import org.rest.sec.persistence.service.impl.RoleServiceUnitTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PrincipalServiceUnitTest.class, RoleServiceUnitTest.class, PrivilegeServiceUnitTest.class, ParseQueryStringUnitTest.class, ConstructQueryStringUnitTest.class })
+@SuiteClasses({ PrincipalServiceUnitTest.class, RoleServiceUnitTest.class, PrivilegeServiceUnitTest.class, ConstructQueryStringUnitTest.class })
 public final class UnitTestSuite {
     //
 }
