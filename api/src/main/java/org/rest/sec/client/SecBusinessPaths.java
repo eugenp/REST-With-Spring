b40fdfacfcaac1c7dd864e11ed08dd2cb25e59a9
eugenp <hanriseldon@gmail.com>
2012-08-20 11:27:53 +0300

    path simplification

--- api/src/main/java/org/rest/sec/client/SecBusinessPaths.java
+++ api/src/main/java/org/rest/sec/client/SecBusinessPaths.java
@@ -1,7 +1,9 @@
 package org.rest.sec.client;
 
+import org.rest.common.client.template.CommonPaths;
 import org.rest.sec.web.common.URIMappingConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,33 +11,40 @@ import org.springframework.stereotype.Component;
 @Profile("client")
 public final class SecBusinessPaths {
 
+    @Value("${http.sec.path}")
+    private String secPaths;
+
     @Autowired
-    SecPaths secPaths;
+    private CommonPaths commonPaths;
 
     // API
 
-    public final String getUserUri() {
-        return secPaths.getRootUri() + URIMappingConstants.USERS;
-    }
-
-    public final String getPrivilegeUri() {
-        return secPaths.getRootUri() + URIMappingConstants.PRIVILEGES;
-    }
-
-    public final String getRoleUri() {
-        return secPaths.getRootUri() + URIMappingConstants.ROLES;
-    }
-
-    public final String getAuthenticationUri() {
-        return secPaths.getRootUri() + "authentication";
-    }
-
-    public final String getLoginUri() {
-        return secPaths.getContext() + "/j_spring_security_check";
+    public final String getContext() {
+        return commonPaths.getServerRoot() + secPaths;
     }
 
     public final String getRootUri() {
-        return secPaths.getRootUri();
+        return getContext() + "/api/";
+    }
+
+    public final String getUserUri() {
+        return getRootUri() + URIMappingConstants.USERS;
+    }
+
+    public final String getPrivilegeUri() {
+        return getRootUri() + URIMappingConstants.PRIVILEGES;
+    }
+
+    public final String getRoleUri() {
+        return getRootUri() + URIMappingConstants.ROLES;
+    }
+
+    public final String getAuthenticationUri() {
+        return getRootUri() + "authentication";
+    }
+
+    public final String getLoginUri() {
+        return getContext() + "/j_spring_security_check";
     }
 
 }
