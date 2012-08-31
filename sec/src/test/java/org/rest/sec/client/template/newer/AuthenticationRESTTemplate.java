16dd2b2a42ce432583d677ada391a840279be235
eugenp <hanriseldon@gmail.com>
2012-08-31 12:24:50 +0300

    maven upgrades and minor authentication template work

--- sec/src/test/java/org/rest/sec/client/template/newer/AuthenticationRESTTemplate.java
+++ sec/src/test/java/org/rest/sec/client/template/newer/AuthenticationRESTTemplate.java
@@ -1,7 +1,7 @@
 package org.rest.sec.client.template.newer;
 
-import org.apache.commons.codec.binary.Base64;
 import org.rest.common.client.marshall.IMarshaller;
+import org.rest.common.web.util.HeaderUtil;
 import org.rest.sec.client.SecBusinessPaths;
 import org.rest.sec.model.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +19,18 @@ public class AuthenticationRESTTemplate {
 
     @Autowired
     IMarshaller marshaller;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
     private SecBusinessPaths paths;
 
-    //
+    public AuthenticationRESTTemplate() {
+        super();
+    }
+
+    // API
 
     public final ResponseEntity<User> authenticate(final String username, final String password) {
         return restTemplate.exchange(getURI(), HttpMethod.POST, new HttpEntity<User>(createHeaders(username, password)), User.class);
@@ -33,26 +38,12 @@ public class AuthenticationRESTTemplate {
 
     // util
 
-    final HttpHeaders createHeaders(final String username, final String password) {
-        return new HttpHeaders() {
-            {
-                set(com.google.common.net.HttpHeaders.ACCEPT, marshaller.getMime());
-
-                final String basicAuthorizationHeader = createBasicAuthenticationAuthorizationHeader(username, password);
-                set(com.google.common.net.HttpHeaders.AUTHORIZATION, basicAuthorizationHeader);
-            }
-        };
-    }
-
     final String getURI() {
         return paths.getAuthenticationUri();
     }
 
-    final String createBasicAuthenticationAuthorizationHeader(final String username, final String password) {
-        final String authorisation = username + ":" + password;
-        final byte[] encodedAuthorisation = Base64.encodeBase64(authorisation.getBytes());
-        final String basicAuthorizationHeader = "Basic " + new String(encodedAuthorisation);
-        return basicAuthorizationHeader;
+    final HttpHeaders createHeaders(final String username, final String password) {
+        return HeaderUtil.createAcceptAndBasicAuthHeaders(marshaller, username, password);
     }
 
 }
