505647a02dfa35e00f8b5bb424374094b0e07849
eugenp <hanriseldon@gmail.com>
2012-11-26 18:26:58 +0200

    minor interface work with generics

---
+++ common/src/main/java/org/rest/common/IRawOperations.java
@@ -0,0 +1,58 @@
+package org.rest.common;
+
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Triple;
+import org.rest.common.search.ClientOperation;
+
+public interface IRawOperations<T> {
+
+    // find - one
+
+    T findOne(final long id);
+
+    /**
+     * - contract: if nothing is found, an empty list will be returned to the calling client <br>
+     */
+    List<T> findAll();
+
+    /**
+     * - contract: if nothing is found, an empty list will be returned to the calling client <br>
+     */
+    List<T> findAllSorted(final String sortBy, final String sortOrder);
+
+    /**
+     * - contract: if nothing is found, an empty list will be returned to the calling client <br>
+     */
+    List<T> findAllPaginated(final int page, final int size);
+
+    /**
+     * - contract: if nothing is found, an empty list will be returned to the calling client <br>
+     */
+    List<T> findAllPaginatedAndSorted(final int page, final int size, final String sortBy, final String sortOrder);
+
+    // create
+
+    T create(final T resource);
+
+    // update
+
+    void update(final T resource);
+
+    // delete
+
+    void delete(final long id);
+
+    void deleteAll();
+
+    // count
+
+    long count();
+
+    // search
+
+    List<T> searchAll(final Triple<String, ClientOperation, String>... constraints);
+
+    T searchOne(final Triple<String, ClientOperation, String>... constraints);
+
+}
