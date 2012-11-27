83563b99b5216b06327168b0f0e6f334d532e94c
eugenp <hanriseldon@gmail.com>
2012-11-27 12:31:00 +0200

    using full URIs in Location header

--- sec/src/main/java/org/rest/sec/web/controller/RedirectController.java
+++ sec/src/main/java/org/rest/sec/web/controller/RedirectController.java
@@ -1,5 +1,7 @@
 package org.rest.sec.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.rest.sec.web.common.UriMappingConstants;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,9 +21,10 @@ public class RedirectController {
     // API
 
     @RequestMapping(value = UriMappingConstants.Singural.PRIVILEGE)
-    public ResponseEntity<Void> privilegesToPrivilege() {
+    public ResponseEntity<Void> privilegesToPrivilege(final HttpServletRequest request) {
+        final String correctUri = request.getRequestURL().toString() + "s";
         final HttpHeaders responseHeaders = new org.springframework.http.HttpHeaders();
-        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, UriMappingConstants.PRIVILEGES);
+        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, correctUri);
 
         final ResponseEntity<Void> redirectResponse = new ResponseEntity<Void>(responseHeaders, HttpStatus.MOVED_PERMANENTLY);
 
@@ -29,9 +32,10 @@ public class RedirectController {
     }
 
     @RequestMapping(value = UriMappingConstants.Singural.ROLE)
-    public ResponseEntity<Void> rolesToRole() {
+    public ResponseEntity<Void> rolesToRole(final HttpServletRequest request) {
+        final String correctUri = request.getRequestURL().toString() + "s";
         final HttpHeaders responseHeaders = new org.springframework.http.HttpHeaders();
-        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, UriMappingConstants.ROLES);
+        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, correctUri);
 
         final ResponseEntity<Void> redirectResponse = new ResponseEntity<Void>(responseHeaders, HttpStatus.MOVED_PERMANENTLY);
 
@@ -39,9 +43,10 @@ public class RedirectController {
     }
 
     @RequestMapping(value = UriMappingConstants.Singural.USER)
-    public ResponseEntity<Void> usersToUser() {
+    public ResponseEntity<Void> usersToUser(final HttpServletRequest request) {
+        final String correctUri = request.getRequestURL().toString() + "s";
         final HttpHeaders responseHeaders = new org.springframework.http.HttpHeaders();
-        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, UriMappingConstants.USERS);
+        responseHeaders.add(org.apache.http.HttpHeaders.LOCATION, correctUri);
 
         final ResponseEntity<Void> redirectResponse = new ResponseEntity<Void>(responseHeaders, HttpStatus.MOVED_PERMANENTLY);
 
