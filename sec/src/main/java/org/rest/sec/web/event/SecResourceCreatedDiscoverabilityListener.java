cf8f03ec6d6016616dc8e646492b2ca922284c77
Eugen <eparaschiv@optaros.com>
2013-05-31 13:00:02 +0300

    HATEOAS fix

---
+++ sec/src/main/java/org/rest/sec/web/event/SecResourceCreatedDiscoverabilityListener.java
@@ -0,0 +1,15 @@
+package org.rest.sec.web.event;
+
+import org.rest.common.web.event.ResourceCreatedDiscoverabilityListener;
+import org.springframework.stereotype.Component;
+
+@Component
+class SecResourceCreatedDiscoverabilityListener extends ResourceCreatedDiscoverabilityListener {
+
+    public SecResourceCreatedDiscoverabilityListener() {
+        super();
+    }
+
+    // API
+
+}
