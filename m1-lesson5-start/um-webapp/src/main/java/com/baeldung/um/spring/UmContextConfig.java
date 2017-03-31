b5428e451e762b87d9a63450d84e7d8acfc9398d
Eugen Paraschiv <eugen@baeldung.com>
2017-03-31 08:41:38 -0500

    small upgrades and cleanup work

--- m1-lesson5-start/um-webapp/src/main/java/com/baeldung/um/spring/UmContextConfig.java
+++ m1-lesson5-start/um-webapp/src/main/java/com/baeldung/um/spring/UmContextConfig.java
@@ -1,13 +1,11 @@
 package com.baeldung.um.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
-@ComponentScan({ "com.baeldung.um.model" })
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @PropertySource({ "classpath:settings.properties" })
 public class UmContextConfig {
