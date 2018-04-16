66fc07f946b613ec5212d832cfa60c0eb46130d5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-04-16 13:47:02 +0200

    minor boot work and upgrades

--- m6-lesson5/um-ui/src/main/java/com/baeldung/UiApplication.java
+++ m6-lesson5/um-ui/src/main/java/com/baeldung/UiApplication.java
@@ -2,11 +2,6 @@ package com.baeldung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,15 +10,6 @@ public class UiApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(UiApplication.class, args);
-    }
-
-    @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(final HttpSecurity http) throws Exception {
-            http.httpBasic().and().authorizeRequests().antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest().authenticated().and().csrf().disable();
-        }
     }
 
 }
