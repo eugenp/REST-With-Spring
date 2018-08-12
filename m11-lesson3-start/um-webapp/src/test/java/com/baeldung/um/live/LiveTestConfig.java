5f5d3990a69ffbe2f1b1d298bb06218fe533e172
loredana crusoveanu <lore.crusoveanu@gmail.com>
2018-08-12 21:26:07 +0300

    remove commons logging, add live test

---
+++ m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/LiveTestConfig.java
@@ -0,0 +1,10 @@
+package com.baeldung.um.live;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiveTestConfig {
+
+    private final static String BASE_URI = "http://localhost:8082/api";
+
+}
---
+++ m11-lesson3-start/um-webapp/src/test/java/com/baeldung/um/live/WebClientLiveTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.um.live;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { LiveTestConfig.class })
+public class WebClientLiveTest {
+
+}
