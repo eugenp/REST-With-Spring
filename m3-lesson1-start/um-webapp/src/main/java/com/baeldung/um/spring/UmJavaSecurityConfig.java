5efcdd0f47617637c60a055bf70f67067355a46f
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-25 18:05:58 +0200

    m-m3-l1

--- m3-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmJavaSecurityConfig.java
+++ m3-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmJavaSecurityConfig.java
@@ -1,6 +1,34 @@
 package com.baeldung.um.spring;
 
-public class UmJavaSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-    //
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.baeldung.um.security")
+public class UmJavaSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobalAuth(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 }
--- m3-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmSecurityConfig.java
+++ m3-lesson1-start/um-webapp/src/main/java/com/baeldung/um/spring/UmSecurityConfig.java
@@ -11,5 +11,4 @@ public class UmSecurityConfig {
     public UmSecurityConfig() {
         super();
     }
-
 }
