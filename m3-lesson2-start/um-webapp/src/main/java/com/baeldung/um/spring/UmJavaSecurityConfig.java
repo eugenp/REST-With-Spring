20c8ebbab744ecbff06965de952c6e27471bb8f8
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-28 13:37:04 +0200

    enabled global method security

--- m3-lesson2-start/um-webapp/src/main/java/com/baeldung/um/spring/UmJavaSecurityConfig.java
+++ m3-lesson2-start/um-webapp/src/main/java/com/baeldung/um/spring/UmJavaSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @ComponentScan("com.baeldung.um.security")
 public class UmJavaSecurityConfig {
 
